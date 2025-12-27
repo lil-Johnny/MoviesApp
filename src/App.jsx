2da7fae13a4e41b6505425ad9620c40db3a62057
lil-Johnny <172312895+lil-Johnny@users.noreply.github.com>
2025-12-27 12:38:53 +0500

    fixing 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
   method: 'GET',
-  // headers: {
-  //   accept: 'application/json',
-  //   Authorization: `Bearer ${API_KEY}`
-  // }
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
 }
 
 const App = () => {
