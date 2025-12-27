527b9d26317affdce6f68b35c108c4dcd03cad54
lil-Johnny <172312895+lil-Johnny@users.noreply.github.com>
2025-12-27 13:27:57 +0500

    fixing the error

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+
 const API_OPTIONS = {
   method: 'GET',
   headers: {
@@ -39,7 +40,7 @@ const App = () => {
     try {
       const endpoint = query 
       ? `${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
-      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+      : `${API_BASE_URL}/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
